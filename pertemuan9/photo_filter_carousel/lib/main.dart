9c15a7180dc08910d8f21c955336ae8737c61e22
tora180405 <toradigda@gmail.com>
2025-11-08 16:37:11 +0700

    praktikum2-pertemuan9

---
+++ pertemuan9/photo_filter_carousel/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'widget/filter_carousel.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: PhotoFilterCarousel(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
