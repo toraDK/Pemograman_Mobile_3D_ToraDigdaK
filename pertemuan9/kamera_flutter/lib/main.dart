960840cac15485368a24d7cef4fb03f7b0b7a66a
tora180405 <toradigda@gmail.com>
2025-11-08 16:30:50 +0700

    praktikum1

---
+++ pertemuan9/kamera_flutter/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'widget/takepicture_screen.dart';
+
+Future<void> main() async {
+  // Pastikan binding sudah diinisialisasi sebelum memanggil availableCameras()
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Dapatkan daftar kamera yang tersedia di perangkat
+  final cameras = await availableCameras();
+
+  // Pilih kamera pertama (biasanya kamera belakang)
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
---
+++ pertemuan9/kamera_flutter/lib/widget/displaypicture_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Display the Picture - 2341720161')),
+      body: Center(
+        child: Image.file(File(imagePath)),
+      ),
+    );
+  }
+}
